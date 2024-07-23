a4e29958c8f620e8b1c50275ccc91999a408f361
EC2 Default User <ec2-user@ip-172-31-46-143.ap-northeast-1.compute.internal>
2024-07-23 13:40:35 +0000

    2.6commit

--- config/puma.rb
+++ config/puma.rb
@@ -1,9 +1,3 @@
-# Puma can serve each request in a thread from an internal thread pool.
-# The `threads` method setting takes two numbers: a minimum and maximum.
-# Any libraries that use thread pools should be configured to match
-# the maximum value specified for Puma. Default is set to 5 threads for minimum
-# and maximum; this matches the default thread size of Active Record.
-#
 max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
@@ -42,10 +36,12 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
 
-bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
 rails_root = Dir.pwd
+rails_env = ENV.fetch("RAILS_ENV") { "development" }
+bind "unix://#{rails_root}/tmp/sockets/puma.sock"
+
 # 本番環境のみデーモン起動
-if Rails.env.production?
+if rails_env == "production"
   pidfile File.join(rails_root, 'tmp', 'pids', 'puma.pid')
   state_path File.join(rails_root, 'tmp', 'pids', 'puma.state')
   stdout_redirect(
@@ -53,6 +49,6 @@ if Rails.env.production?
     File.join(rails_root, 'log', 'puma-error.log'),
     true
   )
-  # デーモン
-  daemonize
+  # デーモン起動を削除
+  # daemonize
 end
