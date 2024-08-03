af4ddfbbf7951c194a947ff26d6595dc745da2bc
ryotajames <ryota.tokobo@gmail.com>
2024-08-03 03:07:50 +0000

    レイアウト２

--- config/application.rb
+++ config/application.rb
@@ -6,11 +6,13 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
+
 module GameFriend
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
