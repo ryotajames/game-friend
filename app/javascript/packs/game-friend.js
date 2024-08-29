932f7ea62191053d4355c5f69c7f0ffca80207ce
ryotajames <ryota.tokobo@gmail.com>
2024-08-29 08:59:19 +0000

    about.js2

--- app/javascript/packs/game-friend.js
+++ app/javascript/packs/game-friend.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('turbolinks:load', () => {
   const about = document.querySelector('.controller');
   if (about) {
     const keyframes = {opacity: [0, 1]};
@@ -6,4 +6,43 @@ document.addEventListener('DOMContentLoaded', () => {
   } else {
     console.error('Element with class "controller" not found');
   }
+});
+
+
+const animateFade = (entries, obs) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.animate(
+        {
+          opacity: [0, 1],
+          filter: ['blur(.4rem)', 'blur(0)'],
+          transform: ['translateY(4rem)', 'translateY(0)'],
+        },
+        {
+          duration: 2000,
+          easing: 'ease',
+          fill: 'forwards',
+        }
+      );
+      obs.unobserve(entry.target);
+    }
+  });
+};
+document.addEventListener('DOMContentLoaded', () => {
+  const options = {
+  root: null, // ビューポートを基準にする（デフォルト）
+  rootMargin: '0px', // ビューポートからのマージン
+  threshold: 0.8 // 要素の50%が見えたらコールバックを呼び出す
+  };
+
+  const fadeObserver = new IntersectionObserver(animateFade, options);
+
+  const fadeElements = document.querySelectorAll('.fadein');
+  fadeElements.forEach((fadeElement) => {
+    fadeObserver.observe(fadeElement);
+  });
+});
+
+fadeElements.forEach((fadeElement) => {
+  fadeObserver.observe(fadeElement);
 });
