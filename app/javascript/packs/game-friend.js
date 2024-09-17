be8dc0f8bb91e2a808d1d6566ff1d8632c9b48a8
ryotajames <ryota.tokobo@gmail.com>
2024-09-17 08:14:07 +0000

    ユーザー詳細のJS

--- app/javascript/packs/game-friend.js
+++ app/javascript/packs/game-friend.js
@@ -3,8 +3,6 @@ document.addEventListener('turbolinks:load', () => {
   if (about) {
     const keyframes = {opacity: [0, 1]};
     about.animate(keyframes, 2000);
-  } else {
-    console.error('Element with class "controller" not found');
   }
 });
 
